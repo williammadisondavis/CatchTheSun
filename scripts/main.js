db8c8259fa0a7872f16ee4da9824eb9b10d86d03
clinturbin <clinturbin@gmail.com>
2018-08-05 15:34:28 -0400

    added image to info window

--- scripts/main.js
+++ scripts/main.js
@@ -3,31 +3,31 @@ var pins = [
       title: "Stone Mountain",
       position: {lat: 33.8082, lng: -84.1702}, 
       description: "Gummies fruitcake marshmallow chocolate oat cake tootsie roll marshmallow cookie pie. Oat cake bonbon biscuit lemon drops. Bonbon bear claw bonbon cotton candy sweet pie jelly beans.", 
-      image: "images/file-name.jpg" 
+      image: "images/image01.jpg" 
     },
     { 
       title: "Arabia Mountain",
       position: {lat: 33.6651, lng: -84.11182}, 
       description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.", 
-      image: "images/file-name.jpg" 
+      image: "images/image02.jpg" 
     },
     { 
       title: "Glenn Hotel Rooftop Bar",
       position: {lat: 33.759126, lng: -84.394726}, 
       description: "Dragée muffin jujubes. Donut chocolate cake tootsie roll cake danish macaroon sweet. Sweet cake cake chocolate bar danish icing ice cream jelly beans. Marshmallow bear claw jelly.", 
-      image: "images/file-name.jpg" 
+      image: "images/image03.jpg" 
     },
     { 
       title: "Lullwater Park",
       position: {lat: 33.802188, lng: -84.314281}, 
       description: "Jujubes bonbon toffee brownie jelly-o bonbon jelly-o chocolate bar. Jelly-o caramels tiramisu sweet chocolate bar. Cake jelly-o sweet apple pie.", 
-      image: "images/file-name.jpg" 
+      image: "images/image04.jpg" 
     },
     { 
       title: "Morningside Nature Preserve Trail",
       position: {lat: 33.806707, lng: -84.356759}, 
       description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.", 
-      image: "images/file-name.jpg" 
+      image: "images/image05.jpg" 
     },
 ];
 
@@ -50,8 +50,18 @@ var addPinsToMap = function (map, infoWindow) {
             title: pin.title
         });
         google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.setContent(pin.title);
+            infoWindow.setContent(addInfoWindowContent(pin));
             infoWindow.open(map, marker);
         });
     })
 };
+
+var addInfoWindowContent = function (pin) {
+    var infoWindowContent = document.createElement("div");
+    infoWindowContent.classList.add("infoWindow")
+    var infoWindowImage = document.createElement("img");
+    infoWindowImage.classList.add("infoWindowImage");
+    infoWindowImage.setAttribute("src", pin.image);
+    infoWindowContent.appendChild(infoWindowImage);
+    return infoWindowContent;
+}
