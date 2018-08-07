d57b3d9afd8d076b15c57e538df5d752116e6adb
Will Davis <williammadisondavis@gmail.com>
2018-08-07 11:14:20 -0400

    changed markers to be Sunny icons

--- scripts/main.js
+++ scripts/main.js
@@ -3,31 +3,36 @@ var pins = [
       title: "Stone Mountain",
       position: {lat: 33.8082, lng: -84.1702}, 
       description: "Gummies fruitcake marshmallow chocolate oat cake tootsie roll marshmallow cookie pie. Oat cake bonbon biscuit lemon drops. Bonbon bear claw bonbon cotton candy sweet pie jelly beans.", 
-      image: "images/image01.jpg"
+      image: "images/image01.jpg",
+      type: 'sunny' 
     },
     { 
       title: "Arabia Mountain",
       position: {lat: 33.6651, lng: -84.11182}, 
       description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.", 
-      image: "images/image02.jpg" 
+      image: "images/image02.jpg",
+      type: 'sunny'
     },
     { 
       title: "Glenn Hotel Rooftop Bar",
       position: {lat: 33.759126, lng: -84.394726}, 
       description: "Dragée muffin jujubes. Donut chocolate cake tootsie roll cake danish macaroon sweet. Sweet cake cake chocolate bar danish icing ice cream jelly beans. Marshmallow bear claw jelly.", 
-      image: "images/image03.jpg" 
+      image: "images/image03.jpg",
+      type: 'sunny'
     },
     { 
       title: "Lullwater Park",
       position: {lat: 33.802188, lng: -84.314281}, 
       description: "Jujubes bonbon toffee brownie jelly-o bonbon jelly-o chocolate bar. Jelly-o caramels tiramisu sweet chocolate bar. Cake jelly-o sweet apple pie.", 
-      image: "images/image04.jpg" 
+      image: "images/image04.jpg",
+      type: 'sunny'
     },
     { 
       title: "Morningside Nature Preserve Trail",
       position: {lat: 33.806707, lng: -84.356759}, 
       description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.", 
-      image: "images/image05.jpg" 
+      image: "images/image05.jpg",
+      type: 'sunny'
     },
 ];
 
@@ -116,11 +121,19 @@ var listingImageDisplay = function(pin) {
     return listingImage;
 };
 
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+var icons = {
+    sunny: {
+      icon: iconBase + 'sunny.png'
+    },
+  };
+
 var addMarkersToMap = function (infoWindow) {
     pins.forEach( function (pin) {
         var marker = new google.maps.Marker( {
             position: pin.position,
             map: map,
+            icon: icons[pin.type].icon,
             title: pin.title
         });
         google.maps.event.addListener(marker, 'click', function() {
