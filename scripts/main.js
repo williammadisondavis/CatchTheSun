f3ce70df7a577b25c1ba87ca80e77e0e28f26003
Will Davis <williammadisondavis@gmail.com>
2018-08-07 15:09:03 -0400

    Made markerIcon object to scale the marker's size, clickbox, and anchor point

--- scripts/main.js
+++ scripts/main.js
@@ -3,36 +3,31 @@ var pins = [
       title: "Stone Mountain",
       position: {lat: 33.8082, lng: -84.1702}, 
       description: "Gummies fruitcake marshmallow chocolate oat cake tootsie roll marshmallow cookie pie. Oat cake bonbon biscuit lemon drops. Bonbon bear claw bonbon cotton candy sweet pie jelly beans.", 
-      image: "images/image01.jpg",
-      type: 'sunny' 
+      image: "images/image01.jpg" 
     },
     { 
       title: "Arabia Mountain",
       position: {lat: 33.6651, lng: -84.11182}, 
       description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.", 
-      image: "images/image02.jpg",
-      type: 'sunny'
+      image: "images/image02.jpg"
     },
     { 
       title: "Glenn Hotel Rooftop Bar",
       position: {lat: 33.759126, lng: -84.394726}, 
       description: "Dragée muffin jujubes. Donut chocolate cake tootsie roll cake danish macaroon sweet. Sweet cake cake chocolate bar danish icing ice cream jelly beans. Marshmallow bear claw jelly.", 
-      image: "images/image03.jpg",
-      type: 'sunny'
+      image: "images/image03.jpg"
     },
     { 
       title: "Lullwater Park",
       position: {lat: 33.802188, lng: -84.314281}, 
       description: "Jujubes bonbon toffee brownie jelly-o bonbon jelly-o chocolate bar. Jelly-o caramels tiramisu sweet chocolate bar. Cake jelly-o sweet apple pie.", 
-      image: "images/image04.jpg",
-      type: 'sunny'
+      image: "images/image04.jpg"
     },
     { 
       title: "Morningside Nature Preserve Trail",
       position: {lat: 33.806707, lng: -84.356759}, 
       description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.", 
-      image: "images/image05.jpg",
-      type: 'sunny'
+      image: "images/image05.jpg"
     },
 ];
 
@@ -130,10 +125,17 @@ var icons = {
 
 var addMarkersToMap = function (infoWindow) {
     pins.forEach( function (pin) {
+        var image = {
+            url: 'https://maps.google.com/mapfiles/kml/shapes/sunny.png',
+            size: new google.maps.Size(30, 30),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 0),
+            scaledSize: new google.maps.Size(30, 30),
+        };
         var marker = new google.maps.Marker( {
             position: pin.position,
             map: map,
-            icon: icons[pin.type].icon,
+            icon: image,
             title: pin.title
         });
         google.maps.event.addListener(marker, 'click', function() {
