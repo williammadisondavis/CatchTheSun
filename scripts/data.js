9f5cd2064097115a990d351b7597792177933fda
clinturbin <clinturbin@gmail.com>
2018-08-09 08:53:48 -0400

    Added new pin objects and moved to new file

---
+++ scripts/data.js
@@ -0,0 +1,171 @@
+var pinsData = {PINS: [
+    { 
+      title: "Stone Mountain",
+      position: {lat: 33.8082, lng: -84.1702}, 
+      description: "Gummies fruitcake marshmallow chocolate oat cake tootsie roll marshmallow cookie pie. Oat cake bonbon biscuit lemon drops. Bonbon bear claw bonbon cotton candy sweet pie jelly beans.",
+      summary: "Less than like 150 character or something, about a sunset.", 
+      image: "images/stone-mountain.jpg" 
+    },
+    { 
+      title: "Westside Reservoir Park",
+      position: {lat: 33.780010, lng: -84.438305}, 
+      description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.",
+      summary: "Less than like 150 character or something, about a sunset.", 
+      image: "images/image01.jpg"
+    },
+    { 
+      title: "Lionel Hampton Beecher Hills Park",
+      position: {lat: 33.780010, lng: -84.466610}, 
+      description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.",
+      summary: "Less than like 150 character or something, about a sunset.", 
+      image: "images/image02.jpg"
+    },
+    { 
+      title: "Arabia Mountain",
+      position: {lat: 33.6651, lng: -84.11182}, 
+      description: "Pastry cookie caramels pudding. Fruitcake sweet roll jelly-o cookie gummi bears liquorice. Bonbon jelly-o jujubes powder marzipan biscuit topping marshmallow marshmallow.",
+      summary: "Less than like 150 character or something, about a sunset.", 
+      image: "images/arabia-mountain.jpg"
+    },
+    { 
+      title: "Hanalei",
+      position: {lat: 22.2033, lng: -159.4977}, 
+      description: "Dragée muffin jujubes. Donut chocolate cake tootsie roll cake danish macaroon sweet. Sweet cake cake chocolate bar danish icing ice cream jelly beans. Marshmallow bear claw jelly.",
+      summary: "Less than like 150 character or something, about a sunset.", 
+      image: "images/hanalei.jpg"
+    },
+    { 
+      title: "Kennesaw Mountain",
+      position: {lat: 33.9764, lng: -84.5792}, 
+      description: "Jujubes bonbon toffee brownie jelly-o bonbon jelly-o chocolate bar. Jelly-o caramels tiramisu sweet chocolate bar. Cake jelly-o sweet apple pie.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/image04.jpg"
+    },
+    { 
+      title: "Lake Charlotte Nature Prserve",
+      position: {lat: 33.673091, lng: -84.353322}, 
+      description: "Jujubes bonbon toffee brownie jelly-o bonbon jelly-o chocolate bar. Jelly-o caramels tiramisu sweet chocolate bar. Cake jelly-o sweet apple pie.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/image03.jpg"
+    },
+    { 
+      title: "Morningside Nature Preserve Trail",
+      position: {lat: 33.806707, lng: -84.356759}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/image05.jpg"
+    },
+    { 
+      title: "Anaheim Hills",
+      position: {lat: 33.8508, lng: -117.7491}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/anaheim-hills.jpg"
+    },
+    { 
+      title: "Arch Rock",
+      position: {lat: 34.135559, lng: -116.054169}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/arch-rock.jpg"
+    },
+    { 
+      title: "Battery Park",
+      position: {lat: 40.7033, lng: -74.0170}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/battery-park.jpg"
+    },
+    { 
+      title: "Big Bear",
+      position: {lat: 34.2439, lng: -116.9114}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/big-bear.jpg"
+    },
+    { 
+      title: "Camelback Mountain",
+      position: {lat: 33.5151, lng: -111.9619}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/camelback-mountain-phoenix.jpg"
+    },
+    { 
+      title: "Canyonlands National Park",
+      position: {lat: 38.3269, lng: -109.8783}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/canyonlands-national-park.jpg"
+    },
+    { 
+      title: "Chicago",
+      position: {lat: 41.8781, lng: -87.6298}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/chicago.jpg"
+    },
+    { 
+      title: "Fort Myers Beach",
+      position: {lat: 26.4520, lng: -81.9481}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/fort-myers-beach.jpg"
+    },
+    { 
+      title: "Glen Eyrie Springs",
+      position: {lat: 38.8884, lng: -104.8765}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/glen-eyrie-castle-colorado-springs.jpg"
+    },
+    { 
+      title: "Joshua Tree",
+      position: {lat: 33.8734, lng: -115.9010}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/joshua-tree.jpg"
+    },
+    { 
+      title: "Laguna Beach",
+      position: {lat: 33.5427, lng: -117.7854}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/laguna-beach.jpg"
+    },
+    { 
+      title: "La Jolla",
+      position: {lat: 32.8328, lng: -117.2713}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/LaJolla.jpg"
+    },
+    { 
+      title: "Lake Lanier",
+      position: {lat: 34.2210, lng: -83.9691}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/lake-lanier.jpg"
+    },
+    { 
+      title: "Larch Mountain",
+      position: {lat: 45.5310, lng: -122.0904}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/larch-mountain.jpg"
+    },
+    { 
+      title: "Manhattan Beach Pier",
+      position: {lat: 33.8843, lng: -118.4120}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/manhattan-beach-pier.jpg"
+    },
+    { 
+      title: "Nānākuli Beach Park",
+      position: {lat: 21.3775, lng: -158.9427}, 
+      description: "Sugar plum pie chocolate cake cake ice cream gummies marshmallow. Donut liquorice bonbon jelly topping bear claw bear claw icing sweet. Ice cream macaroon halvah dessert pie gummi bears.",
+      summary: "Less than like 150 character or something, about a sunset.",
+      image: "images/nanakuli-beach-park-waianae.jpg"
+    },
+  ]
+  };
